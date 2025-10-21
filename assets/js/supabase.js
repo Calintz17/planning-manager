5382702867976190176dc49f747adeab675e1b2c
Calintz17 <roman.attal@gmail.com>
2025-10-21 10:07:02 +0200

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -5,3 +5,6 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true }
 });
+
+
+
